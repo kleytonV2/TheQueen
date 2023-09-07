7bc821a5f6fcc77cd38a69f9c5ea510b2cbc8be8
kleytonV2 <kleyton@hotmail.es>
2023-09-07 19:01:35 +0200

    Project created, working on movements

---
+++ Assets/commonMovement.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//[RequireComponent(typeof(Rigidbody), typeof(BoxCollider))]
+public class commonMovement : MonoBehaviour
+{
+    //[SerializeField] private RigidBody _rigidbody;
+    //[SerializeField] private FixedJoystick _joystick;
+
+    //[SerializeField] private float _moveSpeed = 0f;
+
+    //private void FixedUpdate()
+    //{
+    //    _rigidbody.velocity = new Vector3(_joystick.Horizontal * _moveSpeed, _rigidbody.velocity.y, _joystick.Vertical * _moveSpeed);
+    //}
+   
+}
